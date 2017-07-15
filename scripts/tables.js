9f710fa5a1add7421fc50c57c4790388850aa2ec
Nazar Tolmachov <tolmachovnazar@gmail.com>
2017-07-15 16:43:29 +0300

    test table functional
    added test functional for table

--- scripts/tables.js
+++ scripts/tables.js
@@ -3,4 +3,67 @@
  */
  var CreateTable = (function() {
     var tables = [];
+
+    function onSuccess(json) {
+        if ($(json).text()){
+            alert('hi');
+        }
+        else {
+            alert(tables);
+            var str = JSON.stringify(tables);
+            /*$.post('tables.json',str);*/
+            alert(str);
+        }
+    };
+    function onError(error) {
+        return error;
+    };
+
+    return {
+        add:function () {
+            alert("dfs");
+            var tablesDiv = $('.tables div');
+           console.log(tablesDiv.length);
+           for (var i = 0; i < tablesDiv.length; i++){
+               var newTable = new Object();
+               newTable.id = tablesDiv[i].id;
+               newTable.free = true;
+               tables.push(newTable);
+           }
+           console.log(tables);
+            var str = JSON.stringify(tables);
+            alert(str);
+            $.ajax({
+                type:"GET",
+                url:"tables.json",
+                success:onSuccess,
+                error:onError()
+            });
+          // a = tablesDiv;
+        }
+        /*chekJson: function () {
+            $.get("tables.json",function () {
+                if (this.find('tables')){
+                    alert("not empty");
+                }
+                else {
+                    $.post("tables.json",tables,function () {
+                        alert('tables');
+                    } )
+                }
+            })
+        }*/
+    };
 }());
+$(function () {
+    CreateTable.add();
+   /* $.get("tables.json",function () {
+        if (this.find('tables')){
+            alert("not empty");
+        }
+        else {
+            $.push
+        }
+    })*/
+});
+//var a;
